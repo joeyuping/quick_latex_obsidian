44ef7dc7ffe70ee6d6e04406d3a665cf0c191185
joeyuping <joe.yuping@gmail.com>
2021-08-28 22:45:17 +0800

    1.0.2

--- README.md
+++ README.md
@@ -3,14 +3,17 @@
 This is a very simple plugin to simplify latex fraction and bracket typing, including:
 * Type two consecutive **\$\$** will automatically shift the cursor inbetween the **\$\$**.
 * Type **"{"** or **"["** will automatically close with **"}"** or **"]"**.
+* Typing expressions after subscript **"_"** or superscript **"^"** symbol then followed by **"space" key** will automatically surround the expressions with **"{}"**.
 * Type **"/"** instead of **\frac{}{}**.  
   E.g Type **\$e^2/2\$** followed by a **"space" key** will automatically replace the expression with **\$\frac{e^2}{2}\$** and display $\frac{e^2}{2}$
 
-**Note:** The repo depends on the latest Obsidian API (obsidian.d.ts) in Typescript Definition format, which is still in early alpha so might break anytime!
-**Note:** This is my first Obsidian Plugin project, bugs might be present. Please use with caution.
+**Note:** 
+* The repo depends on the latest Obsidian API (obsidian.d.ts) in Typescript Definition format, which is still in early alpha so might break anytime!
+* This is my first Obsidian Plugin project, bugs might be present. Please use with caution.
+* Currently do not support multiline latex
 
-### Todo:  
-- [ ] automatically surround subscript and superscript expressions with {}
+Future:
+- [ ] - more robust checking whether cursor is within $$
 
 ### source code
 see https://github.com/joeyuping/quick_latex_obsidian
--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { MarkdownView, Plugin } from 'obsidian';
+import { MarkdownView, Notice, Plugin } from 'obsidian';
 
 export default class QuickLatexPlugin extends Plugin {
 
@@ -32,36 +32,76 @@ export default class QuickLatexPlugin extends Plugin {
 		if (['$',' '].contains(event.key)) {
 			const activeLeaf = this.app.workspace.activeLeaf;
 			if (activeLeaf.view instanceof MarkdownView) {
-				const position = cm.getCursor();
+				
 				switch (event.key) {
 					case '$':
-						const t = cm.getRange({line:position.line,ch:position.ch-1},{line:position.line,ch:position.ch})
-						const t2 = cm.getRange({line:position.line,ch:position.ch},{line:position.line,ch:position.ch+1})
-						const t_2 = cm.getRange({line:position.line,ch:position.ch-2},{line:position.line,ch:position.ch})
+						const position1 = cm.getCursor();
+						const t = cm.getRange({line:position1.line,ch:position1.ch-1},{line:position1.line,ch:position1.ch})
+						const t2 = cm.getRange({line:position1.line,ch:position1.ch},{line:position1.line,ch:position1.ch+1})
+						const t_2 = cm.getRange({line:position1.line,ch:position1.ch-2},{line:position1.line,ch:position1.ch})
 						if (t == '$' && t2 != '$') {
-							cm.setCursor({line: position.line,ch:position.ch-1})
+							cm.setCursor({line: position1.line,ch:position1.ch-1})
 						} else if (t_2 == '$$') {
-							cm.setCursor({line: position.line,ch:position.ch-1})
+							cm.setCursor({line: position1.line,ch:position1.ch-1})
 						};
 						break;
 
 					case ' ':
-						const current_line = cm.getLine(position.line);
-						const last_dollar = current_line.lastIndexOf('$',position.ch-1);
-						if (last_dollar == -1){
+						// check whether cursor is within $$ 
+						if (!withinMath(cm)) {
 							break;
-						};
-						const last_equal = current_line.lastIndexOf('=',position.ch);
-						const last_space = current_line.lastIndexOf(' ',position.ch);
-						let frac = last_space>last_dollar?last_space:last_dollar;
-						frac = last_equal>frac?last_equal:frac;
-						const last_divide = current_line.lastIndexOf('/',position.ch);
+						}
+
+						//TODO - perform the operation closest to cursor first
+						let position = cm.getCursor();
+						const current_line = cm.getLine(position.line);
+
+						// subscript bracketing
+						const last_subscript = current_line.lastIndexOf('_',position.ch);
+						if (last_subscript != -1) {
+							const letter_after_subscript = cm.getRange({line:position.line,ch:last_subscript+1},{line:position.line,ch:last_subscript+2});
+							if (letter_after_subscript != '{') {
+								const last_sup = current_line.indexOf('^',last_subscript)==-1?999:current_line.indexOf('^',last_subscript);
+								const last_divide = current_line.indexOf('/',last_subscript)==-1?999:current_line.indexOf('/',last_subscript);
+								const sub_close_index = Math.min(last_sup, last_divide, position.ch);
+								cm.replaceRange('}',{line:position.line,ch:sub_close_index});
+								cm.replaceRange('{',{line:position.line,ch:last_subscript+1});
+								event.preventDefault();
+							}
+						}
+
+						// superscript bracketing
+						position = cm.getCursor();
+						const last_superscript = current_line.lastIndexOf('^',position.ch);
+						if (last_superscript != -1) {
+							const letter_after_superscript = cm.getRange({line:position.line,ch:last_superscript+1},{line:position.line,ch:last_superscript+2});
+							if (letter_after_superscript != '{') {
+								const last_sub = current_line.indexOf('_',last_superscript)==-1?999:current_line.indexOf('_',last_superscript);
+								const last_divide = current_line.indexOf('/',last_superscript)==-1?999:current_line.indexOf('/',last_superscript);
+								const sup_close_index = Math.min(last_sub, last_divide, position.ch);
+								cm.replaceRange('}',{line:position.line,ch:sup_close_index});
+								cm.replaceRange('{',{line:position.line,ch:last_superscript+1});
+								event.preventDefault();
+							}
+						}
+
+						// \frac replace
+						position = cm.getCursor();
+						const last_dollar = current_line.lastIndexOf('$',position.ch-1);
+						const last_equal = current_line.lastIndexOf('=',position.ch-1);
+						const last_space = current_line.lastIndexOf(' ',position.ch-2);
+						const last_greaterthan = current_line.lastIndexOf('>',position.ch-1)
+						const last_lesserthan = current_line.lastIndexOf('<',position.ch-1)
+						const frac = Math.max(last_equal, last_space, last_greaterthan, last_lesserthan, last_dollar)
+						new Notice(`${frac}`)
+						const last_divide = current_line.lastIndexOf('/',position.ch-1);
 						if (last_divide > frac) {
 							cm.replaceRange('}',{line:position.line,ch:position.ch});
 							cm.replaceRange('}{',{line:position.line,ch:last_divide},{line:position.line,ch:last_divide+1});
 							cm.replaceRange('\\frac{',{line:position.line,ch:frac+1});
 							event.preventDefault();
-						}
+						};
+						break;
 				};
 			};
 		};
@@ -74,26 +114,32 @@ export default class QuickLatexPlugin extends Plugin {
 		if (['{','['].contains(event.key)) {
 			const activeLeaf = this.app.workspace.activeLeaf;
 			if (activeLeaf.view instanceof MarkdownView) {
-				const position = cm.getCursor();
-				const current_line = cm.getLine(position.line);
-				const last_dollar = current_line.lastIndexOf('$',position.ch-1);
-				if (last_dollar == -1){
-					return;
-				};
-				const t = cm.getRange({line:position.line,ch:position.ch-2},{line:position.line,ch:position.ch-1})
-				switch (event.key) {
-					case '{':		
-						if (t != '{') {
-							cm.replaceSelection('}','start')
-						};
-						break;
-					case '[':		
-						if (t != '[') {
-							cm.replaceSelection(']','start')
-						};
-						break;
+				if (withinMath(cm)) {
+					const position = cm.getCursor();
+					const t = cm.getRange({line:position.line,ch:position.ch-2},{line:position.line,ch:position.ch-1})
+					switch (event.key) {
+						case '{':		
+							if (t != '{') {
+								cm.replaceSelection('}','start')
+							};
+							break;
+						case '[':		
+							if (t != '[') {
+								cm.replaceSelection(']','start')
+							};
+							break;
+					};
 				};
 			};
 		};
 	};
+}
+
+function withinMath(cm:CodeMirror.Editor) {
+	// check if cursor within $$
+	const position = cm.getCursor();
+	new Notice(`position: ${position}`)
+	const current_line = cm.getLine(position.line);
+	const last_dollar = current_line.lastIndexOf('$',position.ch-1);
+	return last_dollar!=-1;
 }
