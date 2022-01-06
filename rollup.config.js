008f820af9dd2d137f6fc1b2b63788c39f1babc4
ShadiestGoat <shady@shadygoat.eu>
2022-01-06 16:27:00 +0000

    Removed weird types & added better compilation location

--- rollup.config.js
+++ rollup.config.js
@@ -14,7 +14,7 @@ if you want to view the source visit the plugins github repository
 export default {
   input: 'main.ts',
   output: {
-    dir: '.',
+    dir: './output',
     sourcemap: 'inline',
     sourcemapExcludeSources: isProd,
     format: 'cjs',
